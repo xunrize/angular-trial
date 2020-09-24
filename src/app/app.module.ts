37786242ec7d6e7e4d21aa3e049850f096a42a4d
xunrize <xunrize@gmail.com>
2020-09-24 15:22:23 +0700

    Add login component, add router to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LoginComponent } from './login/login.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+    ])
+     ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent ],
+  bootstrap:    [ AppComponent ],
+  
 })
 export class AppModule { }
